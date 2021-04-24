622ca6c2bc8b91d4412d5980ad465168cd4cb944
masa <masatoshi.sboard@gmail.com>
2021-04-24 21:19:05 +0900

    done b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -4,5 +4,28 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        a: [usize;n],
+        b: [usize;n],
     }
+    // 1 <= x <= 1000 が徐々に狭まるイメージ?
+    // x_min, x_maxで考える
+
+    let mut x_min = 1;
+    let mut x_max = 1000;
+
+    for i in 0..n {
+        if x_min <= a[i] {
+            x_min = a[i];
+        }
+        if b[i] <= x_max {
+            x_max = b[i];
+        }
+    }
+    if x_min > x_max {
+        println!("0");
+        return;
+    }
+
+    println!("{}", x_max - x_min + 1);
 }
